67d74b57ca0c4f34d45e5d482cf3150df068e8a3
Brianne Hostutler <briannehostutler@gmail.com>
2016-05-04 22:39:34 -0400

    updates

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendsArray = [
+	{
+	  "name":"Mickey",
+	  "photo":"https://upload.wikimedia.org/wikipedia/en/d/d4/Mickey_Mouse.png",
+	  "scores":[
+	     5,
+	     1,
+	     4,
+	     4,
+	     5,
+	     1,
+	     2,
+	     5,
+	     4,
+	     1
+      		]
+	}
+]
+
+
+module.exports = friendArray;
--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -1,8 +1,13 @@
+var path = require('path');
 
+	app.get('/survey', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
 
+	app.get('/home', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
 
-app.get('/', function(req, res){
-	
-	//res.send("Welcome to the Star Wars Page!")
-	res.sendFile(path.join(__dirname + '/survey.html'));
-})
+	app.use(function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
