eee17313d4372af8dbadbf824e8cf6fb6531bf9a
Brianne Hostutler <briannehostutler@gmail.com>
2016-05-08 10:37:30 -0400

    updaets

--- server.js
+++ server.js
@@ -14,4 +14,6 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 
+require('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
 
